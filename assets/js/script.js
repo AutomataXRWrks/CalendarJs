76caa1d262886f0e8d21328bc3a08932ffe0b292
akautomatadev <automatalab.contacto@gmail.com>
2023-07-13 09:40:25 -0600

    time css logic & current hour text

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,43 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   var rootEl = $('.container-fluid');
-  var currentDay = dayjs().day() // gets day of current week;
-  var currentHour = dayjs().hour() // gets current hour';
+
+  var currentDay = dayjs().format('MMMM dddd D, YYYY'); // gets day of current week;
+  var currentHour = dayjs().hour(); // gets current hour';
+  var currentHour = 11;
   console.log(currentHour);
-  console.log(currentDay);
+  //console.log(currentDay);
+  
+
   
   var hoursCalendarDay = [
-    '9:00 am',
-    '10:00 am',
-    '11:00 am',
-    '12:00 pm',
-    '01:00 pm',
-    '02:00 pm',
-    '03:00 pm',
-    '04:00 pm',
-    '05:00 pm'
+    '9',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17'
   ];
+
+  var hoursCalendarDayText = [
+    '9',
+    '10',
+    '11',
+    '12',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5'  
+  ]
 
   for (var i = 0; i < hoursCalendarDay.length; i++) {
     // Create a new `<div>` for each ability and its text content
+    var day = $('#currentDay')
+    day.text(currentDay);
     var rowdiv = $('<div>');
     var textHour = $('<div>');
     var textArea = $('<textarea>');
@@ -36,7 +54,11 @@ $(function () {
     rowdiv.attr('id', 'hour-'+i);
     rootEl.append(rowdiv);
     textHour.addClass('col-2 col-md-1 hour text-center py-3');
-    textHour.text(hoursCalendarDay[i]);
+    if (i < 2){
+      textHour.text(hoursCalendarDayText[i]+'AM');
+    } else {
+      textHour.text(hoursCalendarDayText[i]+'PM');
+    }
     rowdiv.append(textHour);
     textArea.addClass('col-8 col-md-10 description');
     textArea.attr('rows', '3');
@@ -46,15 +68,40 @@ $(function () {
     buttons.html('<i class="fas fa-save" aria-hidden="true"></i>');
     rowdiv.append(buttons);
 
+    var row = $('.time-block');
+    if (currentHour == hoursCalendarDay[i]){
+
+      var hourCurr = $('#hour-'+i);
+      hourCurr.addClass('present');
+      hourCurr.children('textarea').text('CurrentHour');    
+    } if (currentHour > hoursCalendarDay[i]) {
+      var hourCurr = $('#hour-'+i);
+      var hourCurrPast = $('#hour-'+(i));
+      hourCurr.addClass('past');
+      hourCurrPast.children('textarea').text('Event that already happened');  
+    } if (currentHour < hoursCalendarDay[i] ){
+ 
+      var hourCurr = $('#hour-'+i);
+      hourCurr.addClass('future');      
+    }
+
   }
-  var hourDay = dayjs().format('H');
+
+
+
+
+
+  
+  
+
+ 
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+    //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
